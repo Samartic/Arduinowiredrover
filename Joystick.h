cb8e91bce9160e7cc04ae7cdb1e498a68cd7a56f
Sarmart <113354976+Samartic@users.noreply.github.com>
2023-06-15 12:02:33 -0400

    Update Joystick.h

--- Joystick.h
+++ Joystick.h
@@ -4,17 +4,20 @@
 #include <Arduino.h>
 
 class Joystick{
-  private:
-    float get_angles(float x, float y);
-    float get_forces(float x, float y);
+ private:
+  void get_angle();
+  void get_forces();
+  uint8_t pinx;
+  uint8_t piny;
 
-  public:
-    Joystick(float x, float y);
-    long x;
-    long y;
-    float angle;
-    float forces;
+ public:
+  Joystick(uint8_t pinX, uint8_t pinY);
+  float x;
+  float y;
+  float angle;
+  float forces;
+  void begin();
+  void update();
 
 };
-
 #endif
