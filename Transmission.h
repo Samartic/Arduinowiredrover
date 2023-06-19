5d0ea372d1faccf654266e3e70d8c1704b8ddfe4
Sarmart <113354976+Samartic@users.noreply.github.com>
2023-06-19 09:12:46 -0400

    Create Transmission.h

---
+++ Transmission.h
@@ -0,0 +1,17 @@
+#ifndef TRANSMISSION_H
+#define TRANSMISSION_H
+
+class Transmission{
+  public:
+  Transmission(float angle float force, bool B1, float X, float Y, bool B2);
+    float a;
+    float f;
+    bool b1;
+    float x;
+    float y;
+    bool b2;
+    void update()
+
+  }
+
+#endif
