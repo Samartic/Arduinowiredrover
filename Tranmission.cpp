8d205817bf1fec3a6e4470072ee3cb73f49892e6
Sarmart <113354976+Samartic@users.noreply.github.com>
2023-06-19 09:13:51 -0400

    Create Tranmission.cpp

---
+++ Tranmission.cpp
@@ -0,0 +1,19 @@
+#include "Transmission.h"
+#include <Arduino.h>
+
+Transmission::Transmission(float A, float F, bool B1, float X, float Y, bool B2){
+  float a = A;
+  float f = F;
+  bool b1 = B1;
+  float x = X;
+  float y = Y;
+  bool b = B2;
+}
+void Transmission::update(){
+  float a = A;
+  float f = F;
+  bool b1 = B1;
+  float x = X;
+  float y = Y;
+  bool b = B2;
+}
