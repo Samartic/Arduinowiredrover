2e62475fc5934afe408484cc6f0d2d485340848e
Sarmart <113354976+Samartic@users.noreply.github.com>
2023-06-24 09:53:41 -0400

    Update camera.cpp

--- lib/camera.cpp
+++ lib/camera.cpp
@@ -1,1 +1,18 @@
+#include "Camera.h"
+#include "MotorDriver.h"
+
+Camera::Camera(MotorDriver val_x, MotorDriver val_y){
+  MotorDriver x = val_x;
+  MotorDriver y = val_y;
+}
+
+void Camera::begin(){
+  x.begin();
+  y.begin();
+}
+
+void Camera::move(float fx, float fy){
+  x.move(fx);
+  y.move(fy);
+}
 
