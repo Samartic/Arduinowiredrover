236a101a369605ba595ad86ec1326daab6884293
Sarmart <113354976+Samartic@users.noreply.github.com>
2023-06-24 09:54:16 -0400

    Update camera.h

--- lib/camera.h
+++ lib/camera.h
@@ -1,1 +1,18 @@
+#ifndef CAMERA_H
+#define CAMERA_H
 
+#include "MotorDriver.h"
+
+class camera {
+  private:
+    MotorDriver x;
+    MotorDriver y;
+
+  public:
+    void Camera(MotorDriver val_x, MotorDriver val_y)
+    void begin();
+    void move(float x, float y);
+};
+
+
+#endif
