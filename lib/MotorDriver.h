4d2fd0da3e7b8b81e2bd026ac86df94fca377683
Sarmart <113354976+Samartic@users.noreply.github.com>
2023-07-10 21:30:28 -0400

    Update MotorDriver.h

--- lib/MotorDriver.h
+++ lib/MotorDriver.h
@@ -1,6 +1,8 @@
 #ifndef MOTORDRIVER_H
 #define MOTORDRIVER_H
 
+#include <Arduino.h>
+
 class MotorDriver{
   private:
     uint8_t speed;
@@ -8,7 +10,7 @@ class MotorDriver{
     uint8_t in2;
 
   public:
-  void MotorDriver(uint8_t Speed, uint8_t In1, uint8_t In2);
+  MotorDriver(uint8_t Speed, uint8_t In1, uint8_t In2);
   void begin();
   void move(float forces);
 };
