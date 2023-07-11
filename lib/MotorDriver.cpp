78368422b76755862377bfefbc60d1d8d18aa24d
Sarmart <113354976+Samartic@users.noreply.github.com>
2023-07-10 21:29:38 -0400

    Update MotorDriver.cpp

--- lib/MotorDriver.cpp
+++ lib/MotorDriver.cpp
@@ -3,7 +3,6 @@
 
 
 //for L298N motordriver
-
 MotorDriver::MotorDriver(uint8_t Speed, uint8_t In1, uint8_t In2){
   speed = Speed;
   in1 = In1;
@@ -22,10 +21,11 @@ void MotorDriver::move(float force){
   if (force < 0){ 
     digitalWrite(in1, LOW);
     digitalWrite(in2, HIGH);
-    analogWrite(speed, map(-force, 0, 100, 0, 255));
+    analogWrite(speed, map(force, -100, 0, 0, 255));
   }
-  else:
+  else {
     digitalWrite(in1, HIGH);
     digitalWrite(in2, LOW);
     analogWrite(speed, map(force, 0,100, 0, 255));
+  }
 }
