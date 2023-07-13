084ff63b449f92969b98059e22a34bcc5095f8dd
Sarmart <113354976+Samartic@users.noreply.github.com>
2023-07-12 22:48:48 -0400

    Update manette.ino

--- manette.ino
+++ manette.ino
@@ -1,30 +1,21 @@
 #include "joystick.h"
 #include "transmission.h"
 
-Joystick rover(A0, A2, 01); // must e validated
-Joystick camera(A4, A5, 02); // must e validated
+Joystick rover(A0, A2, 01); // must be validated
+Joystick camera(A4, A5, 02); // must be validated
 
 
 void setup() {
   rover.begin();
   camera.begin();
   Serial.begin(115200);
-  // put your setup code here, to run once:
-
 }
 
 void loop() {
   
   rover.update();
   camera.update();
-  Transmission t = [rover.angle
-                        , rover.forces
-                        , rover.ispress()
-                        , camera.x
-                        , camera.y
-                        , camera.ispress()
-                        ];
-
+  Transmission t(rover.angle, rover.forces, rover.isengage(), camera.x, camera.y, camera.isengage());
   Serial.write(t, len(t));
 
 }
